e098f9d431fb473a7cbfaead25538bf856b39687
Wilson Liang <zliang@atb.com>
2018-03-16 07:37:17 -0700

    Finished adding the HOC

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -6,9 +6,18 @@ export default function(ComposedComponent) {
     static contextTypes = {
       router: React.PropTypes.object
     }
-    
+
+    componentWillMount() {
+      if (!this.props.authenticated)
+        this.context.router.push("/");
+    }
+
+    componentWillUpdate(nextProps){
+      if (!nextProps.authenticated)
+        this.context.router.push("/");
+    }
+
     render() {
-      console.log(this.context);
       return <ComposedComponent {...this.state} />;
     }
   }
